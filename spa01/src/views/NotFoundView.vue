6cde8c87cfff74f53499fb2c754e8944c8c9520e
Ben Chen <idben.chen@gmail.com>
2026-08-13 15:52:20 +0800

    404 - 找不到頁面

--- spa01/src/views/NotFoundView.vue
+++ spa01/src/views/NotFoundView.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import { useRouter, useRoute } from 'vue-router'
 
-<template></template>
+const router = useRouter()
+const route = useRoute()
+</script>
+
+<template>
+    <h1>404 - 找不到頁面</h1>
+    <p>路徑 {{ route.path }} - 不存在</p>
+    <div class="d-flex">
+        <button @click="router.push('/')" class="btn btn-sm btn-primary ms-auto">回首頁</button>
+    </div>
+</template>
 
 <style scoped></style>
