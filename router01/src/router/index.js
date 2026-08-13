ef7153581f5c0035d79551ca70c5abc75373b5a5
Ben Chen <idben.chen@gmail.com>
2026-08-13 09:32:58 +0800

    建立專案, 建立 view 元件與設定 router

---
+++ router01/src/router/index.js
@@ -0,0 +1,36 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import NewsView from '@/views/NewsView.vue'
+import ContactView from '@/views/ContactView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // 懶載入: 有點擊連進來時才進行戴入的動作
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/news',
+      name: 'news',
+      component: NewsView
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: ContactView
+    },
+  ],
+})
+
+export default router
